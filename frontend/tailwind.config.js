0c28f0a5b1ccdb7a5403c847c01757e519e63cc7
Alan Bubalo <alan.buba5@gmail.com>
2024-08-20 23:02:34 +0200

    updated sponsor scrolling

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './src/index.css'],
   theme: {
     extend: {
       fontFamily: {
@@ -11,15 +11,6 @@ export default {
         'primary-600': '#70c8f2',
         'primary-700': '#60b8e2',
         black: '#1c1c1c',
-      },
-      keyframes: {
-        scroll: {
-          '0%': { transform: 'translateX(0)' },
-          '100%': { transform: 'translateX(calc(-100% - 1rem))' },
-        },
-      },
-      animation: {
-        'marquee-scroll': 'scroll 15s linear infinite',
       },
     },
   },
