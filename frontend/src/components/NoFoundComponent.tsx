e0832c2407051bb8d37e7a7f4112ae53e20e8d21
Alan Bubalo <alan.buba5@gmail.com>
2024-10-27 16:20:38 +0100

    Add NoFoundComponent

---
+++ frontend/src/components/NoFoundComponent.tsx
@@ -0,0 +1,25 @@
+import { HiArrowLeft } from 'react-icons/hi'
+import { TransparentLinkButton } from './ui/LinkButton'
+import SPLogoTransparent from '/sou-program-icon-transparent.svg'
+
+export default function NoFoundComponent() {
+  return (
+    <div className="relative flex flex-1 flex-col items-center justify-center bg-black text-center text-gray-200">
+      <div className="absolute inset-0 flex items-center justify-center opacity-5">
+        <img
+          src={SPLogoTransparent}
+          alt="Sou program logo"
+          className="size-[40rem]"
+          width="500"
+          height="500"
+        />
+      </div>
+      <div className="relative z-10 mx-auto flex max-w-screen-lg flex-col gap-6 px-8 py-36 sm:px-6 lg:px-8">
+        <h3 className="font-brioni text-5xl text-white">404 Not found</h3>
+        <div className="flex justify-center">
+          <TransparentLinkButton to="/" icon={<HiArrowLeft />} label="Nazad na početnu stranicu" />
+        </div>
+      </div>
+    </div>
+  )
+}
--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,38 +1,16 @@
-import { Outlet, createRootRouteWithContext } from '@tanstack/react-router';
+import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
 // import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import Footer from '@/components/Footer';
-import NavBar from '@/components/NavBar';
-import ScrollToTop from '@/components/ScrollToTop';
-import { TransparentLinkButton } from '@/components/ui/LinkButton';
-import type { QueryClient } from '@tanstack/react-query';
-import { HiArrowLeft } from 'react-icons/hi';
-import SPLogoTransparent from '/sou-program-icon-transparent.svg';
+import Footer from '@/components/Footer'
+import NavBar from '@/components/NavBar'
+import NoFoundComponent from '@/components/NoFoundComponent'
+import ScrollToTop from '@/components/ScrollToTop'
+import type { QueryClient } from '@tanstack/react-query'
 
 export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()({
   component: RootComponent,
-  notFoundComponent: () => {
-    return (
-      <div className="flex flex-col justify-center">
-        <div className="relative bg-black text-center text-gray-200">
-          <div className="absolute inset-0 flex items-center justify-center opacity-5">
-            <img src={SPLogoTransparent} alt="Sou program logo" className="h-[40rem] w-[40rem]" />
-          </div>
-          <div className="relative z-10 mx-auto flex max-w-screen-lg flex-col px-8 py-36 sm:px-6 lg:px-8">
-            <h3 className="pb-4 font-brioni text-3xl text-white">404 Not found.</h3>
-            <div className="flex justify-center">
-              <TransparentLinkButton
-                to="/"
-                icon={<HiArrowLeft />}
-                label="Nazad na početnu stranicu"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  },
-});
+  notFoundComponent: NoFoundComponent,
+})
 
 function RootComponent() {
   return (
@@ -48,5 +26,5 @@ function RootComponent() {
       {/* <ReactQueryDevtools buttonPosition="top-right" /> */}
       {/* <TanStackRouterDevtools position="bottom-right" /> */}
     </>
-  );
+  )
 }
